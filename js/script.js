a4da5cc4f4727d3e7453dca79323dab38f3e9457
Алексей Слаутин <slaytin2014@yandex.ru>
2021-06-03 21:43:12 +0300

    Выполнил усложненное задание

--- js/script.js
+++ js/script.js
@@ -4,31 +4,59 @@ const isNamber = function (n) {
     return !isNaN(parseFloat(n)) && isFinite(n); 
 };
 
-let numberRandom = Math.floor(Math.random() * 100) + 1;
-console.log(numberRandom);
+function startGame () {
 
-function guessNumber (numbRand) {
+    let numberRandom = Math.floor(Math.random() * 100) + 1;
+    console.log(numberRandom);
 
-    let numberUser = prompt('Угадай число от 1 до 100!');
-    
-    if (numberUser === null){
-        alert(' "Игра окончена" ');
-        return;
+    let attempt = 10;
+
+    function guessNumber (numbRand , x) {
+
+        console.log(x);
+        let numberUser = prompt('Угадай число от 1 до 100!');
+        
+        if (numberUser === null){
+            alert(' "Игра окончена" ');
+            return;
+        }
+
+
+        if (!isNamber(numberUser)) {
+            alert('Введите ЧИСЛО!');
+            guessNumber (numbRand, x);
+        } else if (x === 1) {
+            let loos = confirm('"Попытки закончились, хотите сыграть еще?"');
+            if (loos === false){
+                alert('Игра окончена!');
+                return;
+            } else {
+                startGame();
+            }
+            return;
+        } else if (numberUser > numbRand){
+            x --;
+            alert('Загаданное число меньше, осталось попыток: ' + x);
+            guessNumber (numbRand, x);
+        } else if (numberUser < numbRand){
+            x --;
+            alert('Загаданное число больше, осталось попыток: ' + x);
+            guessNumber (numbRand, x);
+        } else {
+            let winn = confirm (' Поздравляю, Вы угадали!!! Хотели бы сыграть еще? ');
+            if (winn === false){
+                alert('Игра окончена!');
+                return;
+            } else {
+                startGame();
+            }
+            
+        }
+
     }
 
-    if (!isNamber(numberUser)) {
-        alert('Введите ЧИСЛО!');
-        guessNumber (numbRand);
-    } else if (numberUser > numbRand){
-        alert('Загаданное число меньше!');
-        guessNumber (numbRand);
-    } else if (numberUser < numbRand){
-        alert('Загаданное число больше!');
-        guessNumber (numbRand);
-    } else {
-        alert (' "Поздравляю, Вы угадали!!!" ');
-    }
+    guessNumber (numberRandom, attempt);
 
 }
 
-guessNumber (numberRandom);
+startGame();
